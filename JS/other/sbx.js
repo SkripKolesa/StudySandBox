e02ccc9fa6279de7ed4e4ba8e24ac2f3fa291526
artem.novikov <biokako@gmail.com>
2021-10-18 14:33:49 +0400

    sbx

---
+++ JS/other/sbx.js
@@ -0,0 +1,14 @@
+﻿const metadata = {
+  title: 'Scratchpad',
+  translations: [
+    {
+      locale: 'de',
+      localization_tags: [],
+      last_edit: '2014-04-14T08:43:37',
+      url: '/de/docs/Tools/Scratchpad',
+      title: 'Javascript-Umgebung'
+    }
+  ],
+  url: '/en-US/docs/Tools/Scratchpad'
+};
+
