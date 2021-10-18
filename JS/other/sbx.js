a9a5c25c33b60eff274ab45b54eba28353c543d2
skripKolesa <biokako@gmail.com>
2021-10-18 15:25:24 +0400

    reading

--- JS/other/sbx.js
+++ JS/other/sbx.js
@@ -1,15 +1,4 @@
-﻿const metadata = {
-  title: 'Scratchpad',
-  translations: [
-    {
-      locale: 'de',
-      localization_tags: [],
-      last_edit: '2014-04-14T08:43:37',
-      url: '/de/docs/Tools/Scratchpad',
-      title: 'Javascript-Umgebung'
-    }
-  ],
-  url: '/en-US/docs/Tools/Scratchpad'
-};
-
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+﻿const key = 'z';
+const obj = {z: 9};
+const {[key]: foo} = obj;
+console.log(foo);
