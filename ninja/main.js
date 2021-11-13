3a824f9b72dfdb401832a14fd15e7bd9fb47f9d9
skripKolesa <biokako@gmail.com>
2021-11-13 10:45:04 +0400

    iteration

--- ninja/main.js
+++ ninja/main.js
@@ -1,13 +1,19 @@
 ﻿import {assert, report} from "./testSuite.js"
 
 window.onload = function () {
-  function* WeaponG(){
+  function* WeaponG() {
     yield 'Katana';
     yield 'Wakizashi';
-    yield 'Kusarigama';
   }
 
-  for (let weapon of WeaponG()){
-    assert(weapon !== undefined, weapon);
-  }
+  const weaponIterator = WeaponG();
+
+  const result1 = weaponIterator.next();
+  assert(typeof result1 === 'object' && result1.value === 'Katana' && !result1.done, 'Katana');
+
+  const result2 = weaponIterator.next();
+  assert(typeof result2 === 'object' && result2.value === 'Wakizashi' && !result2.done, 'Wakizashi');
+
+  const result3 = weaponIterator.next();
+  assert(typeof result3 === 'object' && result3.value === undefined && result3.done, 'Done');
 }
