249e7e8d7ecf52eb85a1d25f34668dc406229f7b
artem.novikov <biokako@gmail.com>
2021-11-13 10:12:08 +0400

    fun*

--- ninja/main.js
+++ ninja/main.js
@@ -1,4 +1,13 @@
 ﻿import {assert, report} from "./testSuite.js"
 
 window.onload = function () {
+  function* WeaponG(){
+    yield 'Katana';
+    yield 'Wakizashi';
+    yield 'Kusarigama';
+  }
+
+  for (let weapon of WeaponG()){
+    assert(weapon !== undefined, weapon);
+  }
 }
