2ae789762bed6d041bc158c19bb801e70841e5d0
skripKolesa <biokako@gmail.com>
2021-11-29 18:32:40 +0400

    swingswungnnja

--- ninja/main.js
+++ ninja/main.js
@@ -1,6 +1,28 @@
 ﻿import {assert, fail, report} from "./testSuite.js"
 
 window.onload = function () {
+  function Ninja(){
+    this.swung = true;
+  }
 
+  const n1 = new Ninja();
+
+  Ninja.prototype.swingSword = function(){
+    return this.swung;
+  };
+
+  assert(n1.swingSword(), "Method exists even out of order");
+  Ninja.prototype = {
+    pierce: function(){
+      return true;
+    }
+  };
+
+  assert(n1.swingSword(), "n1 can still swing");
+  assert(!("pierce" in n1), "n1 cant pierce");
+
+  const n2 = new Ninja();
+  assert(n2.pierce(), "n2 can pierce");
+  assert(!("swingSword" in n2), "n2 can't swing");
 }
 
